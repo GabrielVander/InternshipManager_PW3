333e98eacb4e9c80472d152d0c50dc6102bc7114
Gabriel Vander <gabsvander@hotmail.com>
2019-11-13 17:53:33 -0300

    DAOs basic configuration

--- src/main/java/org/internship/DAO/StudentDAO.java
+++ src/main/java/org/internship/DAO/StudentDAO.java
@@ -3,4 +3,7 @@ package org.internship.DAO;
 import org.internship.model.Student;
 
 public class StudentDAO extends HibernateDao<Student> {
+  public StudentDAO() {
+    super(Student.class);
+  }
 }
