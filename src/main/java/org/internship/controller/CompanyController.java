7b2d71794e9a2bc06d87bfe1e2f84229eb5c8562
Gabriel Vander <gabsvander@hotmail.com>
2019-11-21 16:39:16 -0300

    CompanyController initial implementation

---
+++ src/main/java/org/internship/controller/CompanyController.java
@@ -0,0 +1,43 @@
+package org.internship.controller;
+
+import org.internship.DAO.CompanyDAO;
+import org.internship.model.Company;
+import org.internship.model.Company;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CompanyController {
+  private CompanyDAO dao = new CompanyDAO();
+
+  @GetMapping("/companies")
+  public List<Company> all(){
+    return (List<Company>) dao.findAll();
+  }
+
+  @PostMapping("/companies")
+  public Company newCompany(@RequestBody Company newCompany){
+    return dao.save(newCompany);
+    //TODO: Implement error handling
+  }
+
+  @GetMapping("/companies/{id}")
+  Company one(@PathVariable Long id) {
+    return dao.find(id);
+    //TODO: Implement error handling
+  }
+
+  @PutMapping("/companies/{id}")
+  Company editCompany(@RequestBody Company company, @PathVariable Long id) {
+    company.setId(id);
+    return dao.edit(company);
+    //TODO: Implement error handling
+  }
+
+  @DeleteMapping("/companies/{id}")
+  Boolean deleteCompany(@PathVariable Long id) {
+    return dao.delete(id);
+    //TODO: Implement error handling
+  }
+}
