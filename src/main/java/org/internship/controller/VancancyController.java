7cb9d454d4e57834933f30d759663f2c442d4bcf
Gabriel Vander <gabsvander@hotmail.com>
2019-11-23 11:43:00 -0300

    VacancyController initial implementation

---
+++ src/main/java/org/internship/controller/VancancyController.java
@@ -0,0 +1,42 @@
+package org.internship.controller;
+
+import org.internship.DAO.VacancyDAO;
+import org.internship.model.Vacancy;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class VancancyController {
+  private VacancyDAO dao = new VacancyDAO();
+
+  @GetMapping("/vacancies")
+  public List<Vacancy> all(){
+    return (List<Vacancy>) dao.findAll();
+  }
+
+  @PostMapping("/vacancies")
+  public Vacancy newVacancy(@RequestBody Vacancy newVacancy){
+    return dao.save(newVacancy);
+    //TODO: Implement error handling
+  }
+
+  @GetMapping("/vacancies/{id}")
+  Vacancy one(@PathVariable Long id) {
+    return dao.find(id);
+    //TODO: Implement error handling
+  }
+
+  @PutMapping("/vacancies/{id}")
+  Vacancy editVacancy(@RequestBody Vacancy vacancy, @PathVariable Long id) {
+    vacancy.setId(id);
+    return dao.edit(vacancy);
+    //TODO: Implement error handling
+  }
+
+  @DeleteMapping("/vacancies/{id}")
+  Boolean deleteVacancy(@PathVariable Long id) {
+    return dao.delete(id);
+    //TODO: Implement error handling
+  }
+}
