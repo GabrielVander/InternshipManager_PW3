8d2a5736e53eb811133341c8683c4340cce772ea
Gabriel Vander <gabsvander@hotmail.com>
2019-11-23 11:30:20 -0300

    StudentController initial implementation

---
+++ src/main/java/org/internship/controller/StudentController.java
@@ -0,0 +1,42 @@
+package org.internship.controller;
+
+import org.internship.DAO.StudentDAO;
+import org.internship.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+  private StudentDAO dao = new StudentDAO();
+
+  @GetMapping("/students")
+  public List<Student> all(){
+    return (List<Student>) dao.findAll();
+  }
+
+  @PostMapping("/students")
+  public Student newStudent(@RequestBody Student newStudent){
+    return dao.save(newStudent);
+    //TODO: Implement error handling
+  }
+
+  @GetMapping("/students/{id}")
+  Student one(@PathVariable Long id) {
+    return dao.find(id);
+    //TODO: Implement error handling
+  }
+
+  @PutMapping("/students/{id}")
+  Student editStudent(@RequestBody Student student, @PathVariable Long id) {
+    student.setId(id);
+    return dao.edit(student);
+    //TODO: Implement error handling
+  }
+
+  @DeleteMapping("/students/{id}")
+  Boolean deleteStudent(@PathVariable Long id) {
+    return dao.delete(id);
+    //TODO: Implement error handling
+  }
+}
